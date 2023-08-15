6f490715e35f6fdedcb0d55c4dff21ff4bffe19c
philipphenkel <philipphnkl@gmail.com>
2023-08-15 13:58:02 +0200

    Kommentare 2

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -4,7 +4,7 @@ import HorseRacing.*
 import Slots.*
 import kotlin.math.round
 
-
+// Eine Nachricht, die angezeigt wird, wenn der Spieler das Casino verlässt
 fun leaveMessage() {
     println("\n\n\n\n\n\n\n\n\n\n")
     println(" ######                                 ${GREEN}#######                  ${BLUE}###                                ${RED}######                                     \n" +
@@ -16,6 +16,7 @@ fun leaveMessage() {
             " ${RESET}######  #    # #    # #    # ######    ${GREEN}#        ####  #    #    ${BLUE}### #    # #    # ###### #    #    ${RED}######  ######  ####   ####   ####  #    # ")
 }
 
+// Eine animierte Ladescreen-Nachricht
 fun loadingScreen(color1: String, color2: String, color3: String) {
     println("\n\n\n")
     print(  "$color1 #####                                      $color2#    #                                 $color3 #####                                \n" +
@@ -27,6 +28,7 @@ fun loadingScreen(color1: String, color2: String, color3: String) {
             "$color1 #####    #   #    #   #   #    # #    #    $color2#    #  ####    #   ###### # #    #    $color3 #####  #    #  ####  # #    #  ####$RESET\n\n\n\n\n")
 }
 
+// Animation des Ladescreens
 fun loadingScreenAnimation() {
     repeat(3) {
         loadingScreen(RED, GREEN, BLUE)
@@ -39,6 +41,7 @@ fun loadingScreenAnimation() {
     println("\n\n\n\n\n\n\n\n\n\n")
 }
 
+// Methode zum Erwerb neuer Casinochips
 fun buyNewChips() {
     var userInputMoney: Double
     do {
@@ -57,22 +60,28 @@ fun buyNewChips() {
     Thread.sleep(1000)
 }
 
+// Die Hauptmethode des Programms
 fun main() {
     var newAtCasino: Boolean = false
     do {
         if (!newAtCasino) {
+            // Willkommensanimation und Name des Spielers abfragen
             loadingScreenAnimation()
             print("Willkommen im Syntax Kotlin Casino! Wie ist ihr Name: ")
             name = readln()
             newAtCasino = true
         } else {
+            // Ladescreen und Nachricht für den Rückkehr des Spielers
             loadingScreen(RED, GREEN, BLUE)
             Thread.sleep(1000)
             println("\n\n\n\n\n")
             successMessage("Willkommen zurück in der Casino Lobby!")
         }
+
         var userInputMenu: Int = 0
+
         if (balance > 0) {
+            // Menü für den Spieler anzeigen, wenn Guthaben vorhanden ist
             do {
                 println("Ihr Guthaben beträgt: $balance€\n" +
                         "Ihre Möglichkeiten:\n" +
@@ -93,6 +102,7 @@ fun main() {
                 }
             } while (userInputMenu == 0)
         } else {
+            // Menü für den Spieler anzeigen, wenn kein Guthaben vorhanden ist
             do {
                 println("Ihr Guthaben beträgt: $balance€\n" +
                         "Ihre Möglichkeiten:\n" +
@@ -110,6 +120,7 @@ fun main() {
                 }
             } while (userInputMenu == 0)
         }
+
         when (userInputMenu) {
             1 -> buyNewChips()
             2 -> blackjack()
@@ -117,6 +128,7 @@ fun main() {
             4 -> slotsGame()
             5 -> leaveCasino = true
         }
+
         if (leaveCasino) {
             leaveMessage()
         }
